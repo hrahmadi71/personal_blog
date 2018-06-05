0c63847519723687afe4b6ed643ac5611496cd26
Hamidreza <h.ahmadi60@yahoo.com>
2018-06-05 19:02:52 +0430

    Post model has successfuly added.

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
+    
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+class post(models.Model):
+    title = models.CharField(max_length=70)
+    text = models.CharField(max_length=3750)
+    date = models.DateTimeField('published')
+    user = models.ForeignKey(User, on_delete=models.SET(get_user_model))
+    
